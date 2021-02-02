39e9d9dd4ed3f13c3f15feb13f5959c75e454607
Sharvil Sopal <48448461+sharvil2244@users.noreply.github.com>
2021-02-02 13:07:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask,jsonify,request,render_template
+
+app = Flask(__name__)
+
+stores = [{
+    'name': 'My Store',
+    'items': [{'name':'my item', 'price': 15.99 }]
+}]
+
+#post /store data: {name :}
+@app.route('/store' , methods=['POST'])
+def create_store():
+  request_data = request.get_json()
+  new_store = {
+    'name':request_data['name'],
+    'items':[]
+  }
+  stores.append(new_store)
+  return jsonify(new_store)
+  
+
+#get /store/<name> data: {name :}
+@app.route('/store/<string:name>')
+def get_store(name):
+  for store in stores:
+    if store['name'] == name:
+          return jsonify(store)
+  return jsonify ({'message': 'store not found'})
+
+
+#get /store
+@app.route('/store')
+def get_stores():
+  return jsonify({'stores': stores})
+ 
+
+#post /store/<name> data: {name :}
+@app.route('/store/<string:name>/item' , methods=['POST'])
+def create_item_in_store(name):
+  request_data = request.get_json()
+  for store in stores:
+    if store['name'] == name:
+        new_item = {
+            'name': request_data['name'],
+            'price': request_data['price']
+        }
+        store['items'].append(new_item)
+        return jsonify(new_item)
+  return jsonify ({'message' :'store not found'})
+ 
+
+#get /store/<name>/item data: {name :}
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+  for store in stores:
+    if store['name'] == name:
+        return jsonify( {'items':store['items'] } )
+  return jsonify ({'message':'store not found'})
+
+ 
+
+app.run(port=5000)
